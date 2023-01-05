40fbaa71670a2334b6d170e6b43cb213f511abfd
LuisF <luisfourier10@gmail.com>
2023-01-05 13:34:10 -0500

    Actualizacion V1

---
+++ LecturaCategoria.py
@@ -0,0 +1,12 @@
+import numpy as np 
+import os 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+
+
+
+df1=pd.read_excel('categorias_interesesVENAPP.xlsx')
+df2=pd.read_excel('categoriasVITMapeo.xlsx')
+
+
+print(df2)
